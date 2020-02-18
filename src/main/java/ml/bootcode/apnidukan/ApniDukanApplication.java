71c9b3d4c945808d6f17188e02321df03ae95856
Sunny Batabyal <batabyalsunny@gmail.com>
2020-02-18 12:31:56 +0530

    Boot project setup

---
+++ src/main/java/ml/bootcode/apnidukan/ApniDukanApplication.java
@@ -0,0 +1,13 @@
+package ml.bootcode.apnidukan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApniDukanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApniDukanApplication.class, args);
+	}
+
+}
